91541823c86561b24f95fd618d9fbe5ec68915c6
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-05 14:10:28 -0500

    created component and used state as well as props to change the data dynamically

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Aldwin", age: 18},
+      {name: "Ivan", age: 16},
+      {name: "Pia", age: 3}
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
-        <h1>Hello, I'm a brank spanking new React App!</h1>
+        <h1>Hello, I'm a brand spanking new React App!</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >My brothers are Ivan and Aldwin.</Person>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
