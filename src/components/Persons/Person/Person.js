3c6b512ee21c43222bb020856e92f5e1f5d4ead7
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-08 22:19:09 -0500

    Created a ref and used it to focus an input element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import styles from './Person.module.css';
 
 // HOC
@@ -6,6 +7,15 @@ import withClass from '../../../hoc/withClass';
 import Auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         const { name, age, click, changed, children } = this.props;
@@ -13,10 +23,17 @@ class Person extends Component {
             <Fragment>
                 <p key="i1" onClick={click}> Hi, my name is {name} and I am {age} years old!</p>
                 <p key="i2">{children}</p>
-                <input key="i3" type="" onChange={changed} value={name} /> 
+                <input key="i3" ref={this.inputElementRef} type="" onChange={changed} value={name} /> 
             </Fragment>
         );
     }
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Person, styles.Person);
