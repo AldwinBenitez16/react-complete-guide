6249938ac693b95f0e2d5ffc90922cd26066883f
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-08 21:23:23 -0500

    Creating a hoc to allow rendering of Adjacent elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import styles from './Person.module.css';
 
+// HOC
+import Auxiliary from '../../../hoc/Auxiliary';
+
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         const { name, age, click, changed, children } = this.props;
-        return [
-            <p key="i1" onClick={click}> Hi, my name is {name} and I am {age} years old!</p>,
-            <p key="i2">{children}</p>,
-            <input key="i3" type="" onChange={changed} value={name} /> 
-        ];
+        return (
+            <Auxiliary>
+                <p key="i1" onClick={click}> Hi, my name is {name} and I am {age} years old!</p>
+                <p key="i2">{children}</p>
+                <input key="i3" type="" onChange={changed} value={name} /> 
+            </Auxiliary>
+        );
     }
 };
 
